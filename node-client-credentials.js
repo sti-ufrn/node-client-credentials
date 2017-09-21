80d8480904fe0ea29ca49aa931f439a573e7c3c9
Jean Guerethes <guerethes@info.ufrn.br>
2017-09-21 16:25:26 -0300

    Atualização do script com um exemplo de consulta.

--- node-client-credentials.js
+++ node-client-credentials.js
@@ -1,4 +1,4 @@
-var http = require("http");
+var http = require("https");
 
 var options = {
   "method": "POST",
@@ -7,6 +7,7 @@ var options = {
   "path": "/authz-server/oauth/token?client_id=<client_id>&client_secret=<client_secret>&grant_type=client_credentials"
 };
 
+var body;
 var req = http.request(options, function (res) {
   var chunks = [];
 
@@ -15,9 +16,43 @@ var req = http.request(options, function (res) {
   });
 
   res.on("end", function () {
-    var body = Buffer.concat(chunks);
+    body = Buffer.concat(chunks);
+    var json = JSON.parse(body.toString());
+    var token = json['access_token'];
     console.log(body.toString());
+    getData("/curso/<VERSION>/modalidades-educacao", token);
   });
 });
 
 req.end();
+
+function getData(path, token) {
+  var http = require("https");
+
+  var options = {
+    "method": "GET",
+    "hostname": "apitestes.info.ufrn.br",
+    "port": null,
+    "path": path,
+    "headers": {
+      "authorization": "bearer " + token,
+      "x-api-key": "X-API-KEY",
+    }
+  };
+
+  var body;
+  var req = http.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function () {
+      body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+  });
+
+  req.end();
+} 
